c7316ad337281eec3e7a9dcf0d55ee586f05037a
dbuencam <dbuencam@sfu.ca>
2022-12-21 22:15:33 -0800

    Complete rewrite of /aboutme (now /userinfo).

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -7,10 +7,14 @@ function convert(timestamp){
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('aboutme')
+        .setName('userinfo')
         .setDescription('provides information about the user.'),
 
     async execute(interaction){
+        const userRoles = interaction.member.roles.cache
+                            .map(role => `\`${role.name}\``)
+                            .join(", ");
+
         const embed = new EmbedBuilder()
 				.setColor(0x0099FF)
                 .setTitle('User Information')
@@ -19,9 +23,8 @@ module.exports = {
 					{name: 'Name', value: `${interaction.user.tag} (ID: ${interaction.user.id})`},
 					{name: 'Created', value: `<t:${convert(interaction.user.createdTimestamp)}:D> (<t:${convert(interaction.user.createdTimestamp)}:R>)`},
 					{name: `Nickname in ${interaction.guild.name}`, value: `${interaction.member.nickname}`},
-					{name: `Joined ${interaction.guild.name}`, value: `<t:${convert(interaction.member.joinedTimestamp)}:D> (<t:${convert(interaction.member.joinedTimestamp)}:R>)`}//,
-					//{name: `Roles in ${interaction.guild.name}`, value: `${userRoles}`},
-					//{name: `Permissions in ${interaction.guild.name}`, value: `${userRoles}`},
+					{name: `Joined ${interaction.guild.name}`, value: `<t:${convert(interaction.member.joinedTimestamp)}:D> (<t:${convert(interaction.member.joinedTimestamp)}:R>)`},
+					{name: `Roles in ${interaction.guild.name}`, value: `${userRoles}`},
                 )
                 .setTimestamp()
                 .setFooter({text: 'Powered by Cypress', iconURL: 'https://cdn.discordapp.com/avatars/524785352247083029/6df9128c69fc53c37c723f8c20ec893f.webp'})
