caf9794ce4cb9f49c4c9ea6426d39a09cafcb31a
Diego Buencamino <dbuencam@sfu.ca>
2023-01-16 12:02:05 -0800

    Change scope of 8-ball responses to be within roll8ball().

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,40 +1,40 @@
 const {SlashCommandBuilder} = require('discord.js');
 
-const ansAffirm = [
-	'It is certain.',
-	'It is decidedly so.',
-	'Without a doubt.',
-	'Yes, definitely.',
-	'You may rely on it.',
-	'As I see it, yes.',
-	'Most likely.',
-	'Outlook good.',
-	'Yes.',
-	'Signs point to yes.',
-];
-
-const ansNonCommit = [
-	'Reply hazy, try again.',
-	'Ask again later.',
-	'Better not tell you now.',
-	'Cannot predict now.',
-	'Concentrate and ask again.',
-];
-
-const ansNegate = [
-	'Don\'t count on it.',
-	'My reply is no.',
-	'My sources say no.',
-	'Outlook not so good.',
-	'Very doubtful.',
-];
-
 // roll8ball() implements an extremely rudimentary way of picking, with weights, a random answer.
 function roll8ball() {
 	// The number of [0/1/2]s in the array determines the probability that one of [Yes/Maybe/No] is selected,
 	// where 0 == Yes, 1 == Maybe, and 2 == No.
 	// i.e. eight zeroes == 80% chance of a 'Yes' answer.
 	const n = [0, 0, 0, 0, 0, 0, 0, 1, 2, 2][Math.floor(Math.random() * 10)];
+
+	const ansAffirm = [
+		'It is certain.',
+		'It is decidedly so.',
+		'Without a doubt.',
+		'Yes, definitely.',
+		'You may rely on it.',
+		'As I see it, yes.',
+		'Most likely.',
+		'Outlook good.',
+		'Yes.',
+		'Signs point to yes.',
+	];
+
+	const ansNonCommit = [
+		'Reply hazy, try again.',
+		'Ask again later.',
+		'Better not tell you now.',
+		'Cannot predict now.',
+		'Concentrate and ask again.',
+	];
+
+	const ansNegate = [
+		'Don\'t count on it.',
+		'My reply is no.',
+		'My sources say no.',
+		'Outlook not so good.',
+		'Very doubtful.',
+	];
 
 	switch (n) {
 	case 0: // Affirmative answer
